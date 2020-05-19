690f3a77ad19c34327a025e0018c1e44f469ca56
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-19 16:29:00 +0200

    codice corretto con removeClass!

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ $(document).ready(function() {
                 quadrato_corrente.text(numero_random);
                 if (numero_random <= 5) {
                     quadrato_corrente.addClass('giallo');
+                    quadrato_corrente.removeClass('verde');
+
                 } else {
                     quadrato_corrente.addClass('verde');
+                    quadrato_corrente.removeClass('giallo');
                 }
-
             },
             'error' : function() {
                 alert('Si è verificato un errore!')
