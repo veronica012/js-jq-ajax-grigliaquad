2c6778e5f228a8d4ad5ea7835d92f7d4b4a901aa
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-19 15:30:47 +0200

    esercizio senza bonus finito

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     $('.quadrato').click(function(){
 
+        var quadrato_corrente = $(this);
+
         $.ajax({
 
             'url': 'https://flynn.boolean.careers/exercises/api/random/int',
@@ -9,6 +11,13 @@ $(document).ready(function() {
             'success': function(data) {
                 var numero_random = data.response;
                 console.log('Il numero random generato con chiamata ajax è: ' + numero_random);
+                quadrato_corrente.text(numero_random);
+                if (numero_random <= 5) {
+                    quadrato_corrente.addClass('giallo');
+                } else {
+                    quadrato_corrente.addClass('verde');
+                }
+
             },
             'error' : function() {
                 alert('Si è verificato un errore!')
