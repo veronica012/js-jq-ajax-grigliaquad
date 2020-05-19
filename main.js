0234b0d8fdc5e823258fa62723afab27b1db8a52
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-19 14:52:10 +0200

    al click su uno dei quadrati è generato un num random con ajax

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-    
 
+    $('.quadrato').click(function(){
 
+        $.ajax({
 
+            'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+            'method': 'GET',
+            'success': function(data) {
+                var numero_random = data.response;
+                console.log('Il numero random generato con chiamata ajax è: ' + numero_random);
+            },
+            'error' : function() {
+                alert('Si è verificato un errore!')
+            }
 
+        });
 
-
-
-
-
+    });
 
 });
